623e272017c765dea9f937d5ba0d7b66313cfb8b
quentonsoon08-tech <quenton.soon08@gmail.com>
2026-06-02 17:01:07 +0800

    Update stationery.py

--- stationery.py
+++ stationery.py
@@ -13,16 +13,6 @@
 # CLASS definition - used to create StationeryItem objects
 # -------------------------------------------------------
 class StationeryItem:
- 
-    # def __init__ runs when a new StationeryItem object is created
-    # It stores the item's name, quantity, and price
-    def __init__(self, name, quantity, price):
- 
-        self.name     = str(name)       # str  - name of the stationery item e.g. "Pen"
-        self.quantity = int(quantity)   # int  - number of units in stock e.g. 200
-        self.price    = float(price)    # float - unit price in dollars e.g. 1.20
-        self.is_valid = False           # bool - False until validate() says data is ok
-     
     # -------------------------------------------------------
     # def calculate_total - multiplies quantity (int) by
     # price (float) and returns total_price (float)
@@ -36,4 +26,4 @@ class StationeryItem:
         # Round to 2 decimal places so it displays as a proper dollar amount
         total_price = round(total_price, 2)         # float, e.g. 240.00
  
-        return total_price   # float  
+        return total_price   # float
